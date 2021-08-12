75cf77d07bd145f5ded5dca2164ee995d0941c8a
edmariooliver <edmariooliver@outlook.com>
2021-08-12 19:48:29 -0300

    Alpha v1.0

---
+++ project/Controllers/CarroController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+class CarroController
+{
+    //
+    public function read()
+    {
+
+    }
+
+    //
+    public function delete()
+    {
+
+    }
+
+    //
+    public function update()
+    {
+
+    }
+
+    //
+    public function create()
+    {
+
+    }
+}
---
+++ project/Models/Carro.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+class Carro
+{
+    //
+    public function read()
+    {
+
+    }
+
+    //
+    public function delete()
+    {
+
+    }
+
+    //
+    public function update()
+    {
+
+    }
+
+    //
+    public function create()
+    {
+
+    }
+}
